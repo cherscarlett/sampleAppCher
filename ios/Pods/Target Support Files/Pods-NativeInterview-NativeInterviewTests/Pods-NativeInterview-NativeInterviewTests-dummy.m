b33ed6eaaf85b8149ca6e2576e8c66205c90cf87
yrichard <yorichard@hotmail.com>
2022-02-23 10:17:18 -0800

    remove pods

--- ios/Pods/Target Support Files/Pods-NativeInterview-NativeInterviewTests/Pods-NativeInterview-NativeInterviewTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NativeInterview_NativeInterviewTests : NSObject
-@end
-@implementation PodsDummy_Pods_NativeInterview_NativeInterviewTests
-@end
